e4ffce5cdbed57ed5dd21be959ceaba907743bc0
Taha Burak <73062498+TBurak96@users.noreply.github.com>
2021-01-24 19:28:40 +0300

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class Customer
+    {
+        public int  Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+       
+        public string Job { get; set; }
+       
+
+    }
+}
---
+++ CustomerManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("A Customer Added!");
+        }
+        public void Update()
+        {
+            Console.WriteLine("A Customer Updated!");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("A Customer Deleted!");
+        }
+        public void ToList(Customer customer)
+        {
+            
+
+               
+                Console.WriteLine("Müşteri Id: " + customer.Id);
+                Console.WriteLine("          ");
+                Console.WriteLine("Müşteri İsim ve Soyisim: " + customer.FirstName+ " "+ customer.LastName );
+                Console.WriteLine("          ");
+                Console.WriteLine("Müşteri Şehir: " + customer.City);
+                Console.WriteLine("          ");
+                Console.WriteLine("Müşteri Meslek: " + customer.Job);
+                Console.WriteLine("          ");
+
+
+
+        }
+    
+
+
+
+
+
+
+    }
+}
