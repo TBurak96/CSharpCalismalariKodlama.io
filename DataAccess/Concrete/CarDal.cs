e5b9170a1b80c32482b74d68dd84d8073ea832f8
Taha Burak <73062498+TBurak96@users.noreply.github.com>
2021-02-03 16:27:21 +0300

    Add files via upload

---
+++ DataAccess/Concrete/CarDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class CarDal : ICarDal
+    {
+
+        List<Car> _cars;
+        public CarDal()
+        {
+            _cars = new List<Car> {
+             new Car{ Id=1, BrandId =1, ColorId=111 , ModelYear=1980 , DailyPrice=350, Description="Aile için uygun."},
+             new Car{ Id=2, BrandId =2, ColorId=222, ModelYear=1981 , DailyPrice=550, Description="Rahat bir kullanım deneyimi."},
+             new Car{ Id=3, BrandId =3, ColorId=333, ModelYear=1982 , DailyPrice=450, Description="Hız tutkunları için."},
+             new Car{ Id=4, BrandId =4, ColorId=444, ModelYear=1983 , DailyPrice=250, Description="Ekonomik."},
+             new Car{ Id=5, BrandId =5, ColorId=555, ModelYear=1984 , DailyPrice=650, Description="Klasik."}};
+            
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p=>p.Id==car.Id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(p=>p.Id == Id).ToList();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,73 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
 
-namespace ClassMetotDemo
+namespace ConsoleUI
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
-            
-            
-            
-            CustomerManager customerManager = new CustomerManager();
+            CarManager carManager = new CarManager(new CarDal());
 
-           
-
-            List<Customer> customers = new List<Customer>();
-
-            Customer customer1 = new Customer();
-            Customer customer2 = new Customer();
-            Customer customer3 = new Customer();
-            Customer customer4 = new Customer();
-
-
-            customers.Add(customer1);
-            customers.Add(customer2);
-            customers.Add(customer3);
-            customers.Add(customer4);
-
-
-            customer1.Id = 1;
-            customer1.FirstName = "Ali";
-            customer1.LastName = "Akarsu";
-            customer1.City = "Çanakkale";
-            customer1.Job = "Kaptan";
-
-            customer2.Id = 2;
-            customer2.FirstName = "Volkan";
-            customer2.LastName = "Demirel";
-            customer2.City = "İstanbul";
-            customer2.Job = "Futbolcu";
-
-            customer3.Id = 3;
-            customer3.FirstName = "Tuncay";
-            customer3.LastName = "Şanlı";
-            customer3.City = "İstanbul";
-            customer3.Job = "Futbolcu";
-
-            customer4.Id = 4;
-            customer4.FirstName = "Serhat";
-            customer4.LastName = "Akın";
-            customer4.City = "İstanbul";
-            customer4.Job = "Futbolcu";
-
-
-
-            customerManager.Add();
-            customerManager.Update();
-            customerManager.Delete();
-
-            Console.WriteLine(" ");
-
-            foreach (var customer in customers)
+            foreach (var car in carManager.GetAll())
             {
-
-             customerManager.ToList(customer);
-            
+                Console.WriteLine("Çıkış senesi: " + car.ModelYear+"Açıklama: "+car.Description);
             }
-            Console.ReadLine();
         }
     }
 }
