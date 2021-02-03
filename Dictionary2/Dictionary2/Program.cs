5a5bf42a263eefb40d54bf740f24e6249af9db86
Taha Burak <73062498+TBurak96@users.noreply.github.com>
2021-02-03 12:55:26 +0300

    Add files via upload

---
+++ Dictionary2/Dictionary2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Daha evvelden aynı isimli çalışmam olduğu için isimlendirmeyi bu şekilde yaptım.
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1919, "Kurtuluş Savaşı başladı.");
+            dictionary.Add(1920, "Türkiye Büyük Millet Meclisi kuruldu.");
+            dictionary.Add(1923, "Cumhuriyet ilan edildi.");
+
+            foreach (KeyValuePair<int, string> item in dictionary)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+            
+            dictionary.Remove(1919);
+
+            foreach (KeyValuePair<int, string> item in dictionary)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+          
+
+            dictionary.Clear();
+
+            Console.ReadLine();
+        }
+    }
+}
